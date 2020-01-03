d06b4a05e4a86e9f08906d9fe6b733c8dea6ec5b
MateosDev <35548023+MateosDev@users.noreply.github.com>
2020-01-03 15:48:23 -0600

    still no clue why it wont add

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 INSERT INTO burgers (burger_name, devoured)
-VALUES ("mushroom swiss", true), ("double bacon cheddar burger", false), ("spicy jelapeno burger", false);
+VALUES ("mushroom swiss", false), ("double bacon cheddar burger", false), ("spicy jelapeno burger", false);
 
 SELECT * FROM burgers;
