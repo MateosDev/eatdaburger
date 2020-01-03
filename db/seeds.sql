b8c25a656aee1c0bd4ed62e455d11b295d703811
MateosDev <35548023+MateosDev@users.noreply.github.com>
2020-01-03 11:47:29 -0600

    db, schema, seeds, had to rearrange some files, incorrect structure at first

---
+++ db/seeds.sql
@@ -0,0 +1,39 @@
+  
+USE burgers_db;
+
+   INSERT INTO burgers
+    (name)
+VALUES
+    ('All American Burger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Double Smashed Onion Burger');
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Santa Fe Poblano Burger');
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Pot Roast Patty Melt Burger');
+  INSERT INTO burgers
+    (name)
+VALUES
+    ('Double Bacon Cheddar Burger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Mushroom and Swiss');
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('BLT Burger');
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Avocado Ranch Burger');
+
+--Definitely Hungry Now
+
+SELECT * FROM burgers;
