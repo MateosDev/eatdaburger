5e6bcbd261bc809b006e40a6f8f94ac584d6bea1
MateosDev <35548023+MateosDev@users.noreply.github.com>
2020-01-08 02:56:50 -0600

    still trying to find the errors

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,9 @@ require("dotenv").config();
 
 //the one component I almost forgot
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.mysql//tacq6q6dw9if0qny:vleoug2e6re90q2b@cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ltmij40iiqv93j88
+
+    );
 } else {
   connection = mysql.createConnection({
     host: "localhost",
