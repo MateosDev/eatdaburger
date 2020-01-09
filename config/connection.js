7470ff0b930b0a57705d6facb2eb730a00d79acf
MateosDev <35548023+MateosDev@users.noreply.github.com>
2020-01-08 20:17:53 -0600

    conenction err on heroku

--- config/connection.js
+++ config/connection.js
@@ -34,19 +34,19 @@ module.exports = connection;
 //============================
 
 
-//the one component I almost forgot
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.mysql//tacq6q6dw9if0qny:vleoug2e6re90q2b@cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ltmij40iiqv93j88
+// //the one component I almost forgot
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.mysql//tacq6q6dw9if0qny:vleoug2e6re90q2b@cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ltmij40iiqv93j88
 
-    );
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "TexasLove817817817..",
-    database: "burgers_db"
-  });
-}
+//     );
+// } else {
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "TexasLove817817817..",
+//     database: "burgers_db"
+//   });
+// }
 
 
 
