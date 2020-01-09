6f77925766066829c5645efd41cb8e78ad701ffe
MateosDev <35548023+MateosDev@users.noreply.github.com>
2020-01-08 19:45:29 -0600

    heroku fix, added .dotenv for pw protection, switched to mysql2

--- config/connection.js
+++ config/connection.js
@@ -4,6 +4,36 @@ var connection;
 require("dotenv").config();
 
 
+
+//CONNECTION TO MYSQL
+var mysql = require('mysql2');
+var connection;
+// var keys = require("./keys.js");
+require('dotenv').config();
+//the one component I almost forgot
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: process.env.mysqlpass,
+    database: 'burgers_db'
+  });
+}
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
+module.exports = connection;
+
+//============================
+
+
 //the one component I almost forgot
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.mysql//tacq6q6dw9if0qny:vleoug2e6re90q2b@cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ltmij40iiqv93j88
@@ -17,6 +47,7 @@ if (process.env.JAWSDB_URL) {
     database: "burgers_db"
   });
 }
+
 
 
 connection.connect();
